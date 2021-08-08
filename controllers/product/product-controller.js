8ea1890f17a44ded185e2e94e21c93835e81b84b
sultonbayevogabek <sultonbayevogabek@gmail.com>
2021-08-08 23:29:39 +0500

    comment qoshish qilindi

--- controllers/product/product-controller.js
+++ controllers/product/product-controller.js
@@ -252,6 +252,8 @@ module.exports = class ProductsController {
 
             product.star = stars
 
+            console.log(comments)
+
             res.render("single-product", {
                 title: `Meros | ${product.ru_name}`,
                 product,
