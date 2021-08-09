6f2c68dabad15a9790b8cc358a42f4a51fa7d0fa
Asadbek <zoirovasadbek2002@gmail.com>
2021-08-09 21:39:28 +0500

    single product cart end

--- public/scripts/_single-product-cart-add.js
+++ public/scripts/_single-product-cart-add.js
@@ -1,8 +1,10 @@
 import { openToast, closingToast } from './_toast'
 import singleProductCart from './_single-product-cart';
+import {selectOne} from "./_functions";
 
 export default function () {
-    console.log(true)
+    let headerCartIndicator = selectOne('.header-middle__link--cart-count'),
+        bottomCartIndicator = selectOne('.bottom-nav__link--cart-count')
     try {
         let addBtn = document.querySelector(".single_add_to_cart");
         let btnGroup = document.querySelector(".product-count");
@@ -17,15 +19,16 @@ export default function () {
                 })
             });
             response = await response.json();
-
+            console.log(response)
             if (!response.ok) {
                 openToast('failed', 'Вы не вошли в систему. Пожалуйста, войдите сначала')
                 closingToast()
             } else {
                 addBtn.classList.add('d-none')
-                btnGroup = document.querySelector(".product-count");
                 btnGroup.querySelector("span").textContent = 1;
                 btnGroup.classList.remove("d-none")
+                headerCartIndicator.textContent = response.result.user.productCountInCart + 1
+                bottomCartIndicator.textContent = response.result.user.productCountInCart + 1
             }
         })
     } catch(e) {}
