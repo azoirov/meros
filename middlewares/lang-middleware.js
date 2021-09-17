dee44fa0ca6b78ec94ab95bc000d59456c34598b
Asadbek <zoirovasadbek2002@gmail.com>
2021-09-16 23:59:31 -0700

    checkout total price

--- middlewares/lang-middleware.js
+++ middlewares/lang-middleware.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
     
     req.lang = lang
 
-    let data = await fs.readFile("../data.json", { encoding: "utf-8" });
+    let data = await fs.readFile(path.join(__dirname, "..", "data.json"), { encoding: "utf-8" });
     data = await JSON.parse(data)
 
     req.data = data[`${lang}`]
