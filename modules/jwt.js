bdf5da5f4c00aca568d67c164a6c1928f01ac4e9
Asadbek <zoirovasadbek2002@gmail.com>
2021-09-29 06:28:16 -0700

    Meros

--- modules/jwt.js
+++ modules/jwt.js
@@ -2,13 +2,13 @@ const Jwt = require("jsonwebtoken");
 const { SECRET_WORD } = require("../config");
 
 const generateToken = (data) => Jwt.sign(data, SECRET_WORD),
-   verifyToken = (data) => {
-      try {
-         let token = Jwt.verify(data, SECRET_WORD);
-         return token;
-      } catch (e) {
-         return false;
-      }
-   };
+    verifyToken = (data) => {
+        try {
+            let token = Jwt.verify(data, SECRET_WORD, { expiresIn: "7d" });
+            return token;
+        } catch (e) {
+            return false;
+        }
+    };
 
 module.exports = { generateToken, verifyToken };
